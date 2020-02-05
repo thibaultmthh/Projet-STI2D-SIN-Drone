c3d9f1ab67fd1b13e866d85613003ce292e149e6
Thibault <thibault.mathian@free.fr>
2020-02-05 20:27:56 +0100

    Detectors and test data

--- teststream.py
+++ teststream.py
@@ -3,20 +3,32 @@ import time
 print("cv2 loaded")
 
 
-detector = cv2.CascadeClassifier('detectors/1.xml')
-
+detector = cv2.CascadeClassifier('detectors/face1.xml')
+detector2 = cv2.CascadeClassifier("detectors/profilface.xml")
+detector3 = cv2.CascadeClassifier("detectors/1.xml")
+a = 0
 while True:
-    cap = cv2.VideoCapture("data/2.mp4")
+    cap = cv2.VideoCapture("data/salle2.mp4")
     #cap = cv2.VideoCapture(0)
     while cap.isOpened:
+        a +=1
         time.sleep(0.001)
         ret, frame = cap.read()
-        if ret:
+        print(a)
+        if ret :
             gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-            gray = cv2.resize(gray,(300,300))
-            body = detector.detectMultiScale(gray, 1.04, 2)
+            #gray = cv2.resize(gray,(300,300))
+            body = detector.detectMultiScale(frame, 3, 5)
+            #body2 = detector2.detectMultiScale(frame, 1.7, 5)
+            body3 = detector3.detectMultiScale(frame, 1.06,5)
             for (x,y,w,h) in body:
                 cv2.rectangle(gray, (x,y), (x+w,y+h), (255,0,0), 3)
+            """
+            for (x,y,w,h) in body2:
+                cv2.rectangle(gray, (x,y), (x+w,y+h), (255,0,0), 3)
+            """
+            for (x,y,w,h) in body3:
+                cv2.rectangle(gray, (x,y), (x+w,y+h), (0,255,0), 3)
 
 
             cv2.imshow("win", gray)
